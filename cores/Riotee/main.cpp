9a37a4a7e38dedc88a3f1fda48d63dc0449e584e
Kai Geissdoerfer <kai.geissdoerfer@tu-dresden.de>
2023-04-25 17:31:20 +0200

    add support for startup and bootstrap callbacks

--- cores/Riotee/main.cpp
+++ cores/Riotee/main.cpp
@@ -13,11 +13,19 @@
 #include "timing.h"
 #include "uart.h"
 
+void startup(void);
+void bootstrap(void);
 void setup(void);
 void loop(void);
 
+__attribute__((weak)) void startup(void){};
+__attribute__((weak)) void bootstrap(void){};
+
+/* This gets called very early, before setting up memory */
+void startup_callback(void) { startup(); }
+
 /* This gets called one time after flashing new firmware */
-void bootstrap_callback(void) { printf("All new!"); }
+void bootstrap_callback(void) { bootstrap(); }
 
 /* This gets called after every reset */
 void reset_callback(void) { setup(); }
